5340d110934a8489cb7f8fdc8b6038a2a900b03b
Amirouche Boubekki <boubekki@enseirb.fr>
2009-05-20 21:35:58 +0200

    more data to parse

--- drinks/settings.py
+++ drinks/settings.py
@@ -9,17 +9,13 @@ ADMINS = (
     ('boubekki', 'boubekki@enseirb.fr'),
 )
 
-ROOT_PATH = "/home/amirouche/Desktop/drinks-project"
+ROOT_PATH = "/home/amirouche/Desktop/drink-project"
 ROOT_URL = "http://localhost:8000"
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', '' or 'oracle'.
-DATABASE_NAME = '../mydb.sqlite'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = '../mydb.sqlite'
 
 TIME_ZONE = 'Europe/Paris'
 
--- drinks/urls.py
+++ drinks/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from mydrinks.models import Drink, Comment
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,9 +9,14 @@ admin.autodiscover()
 admin.site.register(Drink)
 admin.site.register(Comment)
 
+DRINKS = {  'queryset': Drink.objects.all(),
+            }
+
 urlpatterns = patterns('',
-    # Example:
-    # (r'^drinks/', include('drinks.foo.urls')),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
+  (r'^$', 'django.views.generic.list_detail.object_list', DRINKS),
+  (r'^drink/(?P<object_id>.*)/$', 'django.views.generic.list_detail.object_detail', DRINKS),
+  (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+  (r'^admin/', include(admin.site.urls)),
+  (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    {'document_root': settings.MEDIA_ROOT, 'show_indexes': True }),
 )
